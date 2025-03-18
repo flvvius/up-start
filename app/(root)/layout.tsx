12e8b16fba140a826f00068ca82424fb44c22b73
flavius <flaviuscojocaru19@gmail.com>
2025-03-18 17:50:35 +0200

    feat: next auth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { signOut, signIn, useSession } from "next-auth/react";
+
+const Navbar = () => {
+  const { data: session } = useSession();
+
+  return (
+    <header className="px-5 font-work-sans bg-white shadow-sm">
+      <nav className="flex items-center justify-between">
+        <Link href="/">
+          <Image src="/upstart-logo.png" alt="logo" width={70} height={70} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={() => signOut()}>
+                <span>Log out</span>
+              </button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => signIn("github")}>
+                <span>Log in</span>
+              </button>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
