ef42e7e4a6370106de3d46bb9d33aaac777f629b
flavius <flaviuscojocaru19@gmail.com>
2025-03-26 17:06:51 +0200

    fix: navbar responsiveness

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,29 +4,42 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { signOut, signIn, useSession } from "next-auth/react";
+import { PlusCircleIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = () => {
   const { data: session } = useSession();
 
   return (
-    <header className="px-5 font-work-sans bg-white shadow-sm">
+    <header className="px-5 py-3 font-work-sans bg-white shadow-sm">
       <nav className="flex items-center justify-between">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={70} height={70} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
         <div className="flex items-center gap-5 text-black">
-          {session && session?.user ? (
+          {session && session?.id ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <span className="sm:hidden">
+                  <PlusCircleIcon className="w-4 h-4" />
+                </span>
               </Link>
               <button
                 onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
               >
-                <span>Log out</span>
+                <span className="max-sm:hidden">Log out</span>
+                <span className="sm:hidden">
+                  <LogOutIcon className="w-4 h-4 text-red-500" />
+                </span>
               </button>
-              <Link href={`/user/${session?.user.name}`}>
-                <span>{session?.user.name}</span>
+              <Link href={`/user/${session.id}`}>
+                <Avatar>
+                  <AvatarImage src={session?.user.image || ""} />
+                  <AvatarFallback>
+                    {session?.user.name?.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
